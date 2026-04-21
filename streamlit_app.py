71e8f1a56a000ee89b889dc1614a9e220f3160c0
yellowlion2 <greenellow2@gmail.com>
2026-04-21 05:41:17 +0000

    자기소개 앱의 기본 구조 추가 및 내용 업데이트

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("자기소개")
+
+st.header("안녕하세요!")
+st.write("저는 [황지연]입니다. [고려대학교 교육대학원 수학교육전공 석사과정중입니다.].")
+
+st.header("경력")
+st.write("- [직업이나 경력을 여기에 적으세요]")
+st.write("- [또 다른 경력을 여기에 적으세요]")
+
+st.header("관심사")
+st.write("- [취미나 관심사를 여기에 적으세요]")
+st.write("- [또 다른 관심사를 여기에 적으세요]")
+
+st.header("연락처")
+st.write("이메일: [이메일 주소를 여기에 넣으세요]")
+st.write("LinkedIn: [LinkedIn 링크를 여기에 넣으세요]")
+st.write("저는 [이름]입니다. [간단한 소개 문구를 여기에 넣으세요].")
+
+st.header("경력")
+st.write("- [직업이나 경력을 여기에 적으세요]")
+st.write("- [또 다른 경력을 여기에 적으세요]")
+
+st.header("관심사")
+st.write("- [취미나 관심사를 여기에 적으세요]")
+st.write("- [또 다른 관심사를 여기에 적으세요]")
+
+st.header("연락처")
+st.write("이메일: [이메일 주소를 여기에 넣으세요]")
+st.write("LinkedIn: [LinkedIn 링크를 여기에 넣으세요]")
